6215941eadd560f06e2b0073378ab26b43b1eea6
Puyan Wei <puyanwei@gmail.com>
2018-02-08 13:03:30 +0000

    setup jasmine

---
+++ spec/first-primes-test.js
@@ -0,0 +1,8 @@
+describe("jasmine works", function() {
+  it("working", function() {
+    expect(true).toBe(true);
+  });
+  it("working", function() {
+    expect(false).not.toBe(true);
+  });
+});
